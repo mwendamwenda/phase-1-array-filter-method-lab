3b502c681177903024a9bffaa014d6110d9208cd
mwendamwenda <benmwenda83@gmail.com>
2022-08-21 15:12:57 +0300

    compeleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(array, string){
+    const names = array.filter(name => name.toLowerCase() === string.toLowerCase());
+ 
+    return names;
+ }
+ 
+ 
+ //findMatching(drivers, "bobby");
+ 
+ function fuzzyMatch(array, string){
+     const driverNames = array.filter(name => name.charAt() === string.charAt());
+ 
+     return driverNames;
+ }
+ 
+ 
+ function matchName(driver, string){  
+     const match = driver.filter(details => details.name === string);
+ 
+     return match;
+ }
+ 
+ console.log(matchName(drivers, "Bobby"));
